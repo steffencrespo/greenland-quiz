306ac77aa1c0f3635eb1452d3d491f5e6bac1796
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-12 22:48:59 -0400

    adds function to compose question html

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 // let quiz = {
 // 	questions: {
 // 		question: {
-// 			text: [''],
+// 			text: '',
+//			answers: [],
 // 			correct: 0,
 // 			googleIt: ''
 // 		},
 // 		question: {
-// 			text: [''],
+// 			text: '',
+//			answers: [],
 // 			correct: 0,
 // 			googleIt: ''
 // 		}
 // 	}
 // }
 
+const QUIZ_START_BUTTON = '.start-btn';
+const QUIZ_QUESTION_BOX = '.question-box';
+const HIDDEN_ELEMENT = 'hidden';
+
 function hideButtonAndDisplayQuestion() {
-	$('.start-btn').click(function(event) {
+	$(QUIZ_START_BUTTON).click(function(event) {
 		event.preventDefault();
-		$(event.currentTarget).addClass('hidden');
-		$('.question-box').removeClass('hidden');
+		$(event.currentTarget).addClass(HIDDEN_ELEMENT);
+		$(QUIZ_QUESTION_BOX).removeClass(HIDDEN_ELEMENT);
 	});
+}
+
+function composeQuestion(question) {
+	let questionText = question.text;
+	let answers = question.answers;
+	let helpFromGoogle = question.googleIt;
+
+	let questionHtml = `
+				<div class="${QUIZ_QUESTION_BOX}">
+				<p>What is your favorite question?</p>
+				<form>
+				  <input type="radio" name="gender" value="male" checked> Male<br>
+				  <input type="radio" name="gender" value="female"> Female<br>
+				  <input type="radio" name="gender" value="other"> Other
+				</form>
+			</div>
+			`
 }
 
 $(function() {
