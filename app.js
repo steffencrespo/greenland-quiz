6975853336cb4978a43f133aa66b4c5a9e447005
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-12 21:37:57 -0400

    adds a googleIt attribute for additional lmgtfy feature

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let quiz = {
 	questions: {
 		question: {
 			text: [''],
-			correct: 0
+			correct: 0,
+			googleIt: ''
 		},
 		question: {
 			text: [''],
-			correct: 0
+			correct: 0,
+			googleIt: ''
 		}
 	}
 }
