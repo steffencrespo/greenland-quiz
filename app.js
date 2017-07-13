3c257a9b18cb9e808fdef504d63fa7861031a63e
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-13 14:23:16 -0400

    fixes click event delegate to make radio btns work

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const QUIZ_START_BUTTON = '.start-btn';
-const QUIZ_QUESTION_BOX = '.quiz-question';
-const QUIZ_QUESTION_FORM = '.quiz-question-form';
+const QUIZ_START_BUTTON = 'start-btn';
+
+const QUIZ_RADIO_BUTTONS = "input[name='answer']";
+const QUIZ_SELECTED_ANSWER_RADIO_BUTTON = "input[name='answer']:checked";
+
+const QUIZ_QUESTION_BOX = 'quiz-question';
+const QUIZ_QUESTION_FORM = 'quiz-question-form';
 const HIDDEN_ELEMENT = 'hidden';
 const QUESTION_SUBMIT_BUTTON = 'quiz-question-submit-btn';
 
 function hideButtonAndDisplayQuestion() {
-	$(QUIZ_START_BUTTON).click(function(event) {
+	$("."+QUIZ_START_BUTTON).click(function(event) {
 		event.preventDefault();
 		$(event.currentTarget).addClass(HIDDEN_ELEMENT);
-		$(QUIZ_QUESTION_BOX).removeClass(HIDDEN_ELEMENT);
+		$("."+QUIZ_QUESTION_BOX).removeClass(HIDDEN_ELEMENT);
 	});
 }
 
@@ -19,21 +23,26 @@ function composeQuestion(question) {
 
 	let questionHtml = `
 				<div class="${QUIZ_QUESTION_BOX}">
-				<p>${questionText}</p>
-				<form class="${QUIZ_QUESTION_FORM}">
-				  <input type="radio" name="answer" value="${answers[0]}"> ${answers[0]}<br>
-				  <input type="radio" name="answer" value="${answers[1]}"> ${answers[1]}<br>
-				  <input type="radio" name="answer" value="${answers[2]}"> ${answers[2]}<br>
-				  <input type="radio" name="answer" value="${answers[3]}"> ${answers[3]}<br>
-				  <input type="radio" name="answer" value="${answers[4]}"> ${answers[4]}<br>
-				  <input class=${QUESTION_SUBMIT_BUTTON} type="submit" value="Next">
-				</form>
+					<p>${questionText}</p>
+					<form class="${QUIZ_QUESTION_FORM}">
+					  <input type="radio" name="answer" value="${answers[0]}"> ${answers[0]}<br>
+					  <input type="radio" name="answer" value="${answers[1]}"> ${answers[1]}<br>
+					  <input type="radio" name="answer" value="${answers[2]}"> ${answers[2]}<br>
+					  <input type="radio" name="answer" value="${answers[3]}"> ${answers[3]}<br>
+					  <input type="radio" name="answer" value="${answers[4]}"> ${answers[4]}<br>
+					  <input class=${QUESTION_SUBMIT_BUTTON} type="submit" value="Next">
+					</form>
 			</div>
 			`
-	$(QUIZ_QUESTION_BOX).html(questionHtml);
+	$("."+QUIZ_QUESTION_BOX).html(questionHtml);
 }
 
 function selectAnswerAndDisplayCorrect() {
+	
+	$("."+QUIZ_QUESTION_BOX).on('click', QUIZ_RADIO_BUTTONS, function(event) {
+		alert($("input[name='answer']:checked").val());
+	});
+
 	// this takes care of handling user click on a radio button
 	// when user clicks an answer it evaluates it and shows whether correct or not
 }
@@ -53,6 +62,7 @@ $(function() {
 	hideButtonAndDisplayQuestion();
 	composeQuestion(questions[0]);
 	loadNextQuestion();
+	selectAnswerAndDisplayCorrect();
 });
 
 let questions = [
