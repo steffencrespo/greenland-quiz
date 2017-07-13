b059bfeb9a96b42c7a7cc2962f4c5ff879a14063
Leonardo Steffen <leosteffen84@gmail.com>
2017-07-12 22:30:23 -0400

    modifies css, html and js to make the button hide itself once clicked

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-let quiz = {
-	questions: {
-		question: {
-			text: [''],
-			correct: 0,
-			googleIt: ''
-		},
-		question: {
-			text: [''],
-			correct: 0,
-			googleIt: ''
-		}
-	}
+// let quiz = {
+// 	questions: {
+// 		question: {
+// 			text: [''],
+// 			correct: 0,
+// 			googleIt: ''
+// 		},
+// 		question: {
+// 			text: [''],
+// 			correct: 0,
+// 			googleIt: ''
+// 		}
+// 	}
+// }
+
+function hideElement() {
+	$('.start-btn').click(function(event) {
+		event.preventDefault();
+		$(event.currentTarget).addClass('hidden');
+	});
 }
+
+$(function() {
+	hideElement();
+});
